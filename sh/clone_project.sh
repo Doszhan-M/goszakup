33be6cea12401ca2d9881e99afb61e30bd72d2db
Doszhan <doszhan.work@gmail.com>
2024-02-22 19:19:46 +0600

    feat sh:

---
+++ sh/clone_project.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Создание директории projects
+sudo mkdir -p /projects
+
+# Назначение прав доступа 777 для директории
+sudo chmod -R 777 /projects
+
+# Установка ACL прав для директории по умолчанию
+sudo setfacl -m d:u::rwx /projects
+sudo setfacl -m d:g::rwx /projects
+sudo setfacl -m d:o::rwx /projects
+
+# Переход в директорию projects и клонирование репозитория
+cd /projects
+git clone git@github.com:Doszhan-M/goszakup.git
+
+# Переход в директорию склонированного репозитория
+cd goszakup
+
+echo "Скрипт выполнен."
