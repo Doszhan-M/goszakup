725ddca713786a1faa5d3ce348cc8c7a0eba8d14
Doszhan <doszhan.work@gmail.com>
2024-07-26 19:11:28 +0500

    feat:restart ncalayer

--- signer/managers/eds.py
+++ signer/managers/eds.py
@@ -153,4 +153,5 @@ class EdsManager:
             websocket.close()
         except ConnectionRefusedError:
             logger.error("NCALayer dont work, waiting....")
+            sleep(0.3)
             return cls.healthcheck_ncalayer()
