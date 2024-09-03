0b21808f295b78b292939e081703e89115a7bce9
Doszhan <doszhan.work@gmail.com>
2024-09-03 19:50:10 +0500

    feat:add pass to swagger

--- tender_pw/app/core/config.py
+++ tender_pw/app/core/config.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv
+from functools import lru_cache
 from pydantic_settings import BaseSettings
 
 
@@ -10,9 +11,16 @@ load_dotenv(f"{BASE_DIR.parent.parent}/env/tender.env")
 
 
 class Settings(BaseSettings):
+    OPENAPI_USER: str = "admin"
+    OPENAPI_PASS: str = "aCiOnIQuArdE"
     HEADLESS_DRIVER: bool = False
     BASE_DIR: str = str(BASE_DIR)
     SIGNER_HOST: str = "127.0.0.1:50051"
 
 
 settings = Settings()
+
+
+@lru_cache()
+def get_settings():
+    return settings
