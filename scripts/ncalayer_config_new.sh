b1709672b70524480d3be72bad721b88bcd0d64c
Doszhan <doszhan.work@gmail.com>
2024-09-19 17:01:08 +0500

    fix:ncalayer script

--- scripts/ncalayer_config_new.sh
+++ scripts/ncalayer_config_new.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Проверка прав на создание файла в /etc/systemd/system
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Этот скрипт должен быть запущен с правами root."
+    exit 1
+fi
 
+# Укажите пользователя, от имени которого будет запускаться сервис
+SERVICE_USER="asus"
 
-# Определяем путь к директории systemd user services
-SYSTEMD_USER_DIR="$HOME/.config/systemd/user"
+# Получаем домашнюю директорию пользователя
+USER_HOME=$(eval echo "~$SERVICE_USER")
 
-# Создаем директорию, если она не существует
-mkdir -p "$SYSTEMD_USER_DIR"
-
-# Определяем путь к файлу сервиса
-SERVICE_FILE="$SYSTEMD_USER_DIR/ncalayer.service"
+SERVICE_FILE="/etc/systemd/system/ncalayer.service"
 
 # Создаем сервисный файл с заданным содержимым
 cat > "$SERVICE_FILE" <<EOF
@@ -19,24 +22,26 @@ After=graphical.target
 
 [Service]
 Type=simple
-ExecStart=/home/asus/Programs/NCALayer/ncalayer.sh --run
-Restart=on-failure
+User=$SERVICE_USER
+Group=$SERVICE_USER
 Environment=DISPLAY=:0
-Environment=HOME=/home/asus
+Environment=HOME=$USER_HOME
+ExecStart=$USER_HOME/Programs/NCALayer/ncalayer.sh --restart
+Restart=on-failure
 
 [Install]
-WantedBy=default.target
+WantedBy=graphical.target
 EOF
 
 echo "Файл $SERVICE_FILE успешно создан."
 
-# Перезагружаем конфигурацию systemd user
-systemctl --user daemon-reload
+# Перезагружаем конфигурацию systemd
+systemctl daemon-reload
 
-# Включаем сервис для автозапуска при входе в систему
-systemctl --user enable ncalayer.service
+# Включаем сервис для автозапуска при загрузке системы
+systemctl enable ncalayer.service
 
 # Запускаем сервис немедленно
-systemctl --user start ncalayer.service
+systemctl start ncalayer.service
 
 echo "Сервис ncalayer запущен и настроен на автозапуск через systemd."
