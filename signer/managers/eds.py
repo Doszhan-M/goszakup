be559a5b8921cbf21624964796407489f6056252
Doszhan <doszhan.work@gmail.com>
2024-09-19 21:48:51 +0500

    screenshot

--- signer/managers/eds.py
+++ signer/managers/eds.py
@@ -21,7 +21,7 @@ if settings.ENVIRONMENT == "TUF17":
 elif settings.ENVIRONMENT == "VIVOBOOK":
     pyautogui_images = settings.BASE_DIR + "/static/vivobook/"
 elif settings.ENVIRONMENT == "SERVER_GNOME":
-    pyautogui_images = settings.BASE_DIR + "/static/server_gnome/"
+    pyautogui_images = settings.BASE_DIR + "/static/server_gnome_2/"
 
 
 class EdsManager:
