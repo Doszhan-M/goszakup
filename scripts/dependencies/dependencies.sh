771bea904eb27d93177ae3de5b36a992a771cdde
Doszhan <doszhan.work@gmail.com>
2024-10-04 14:25:14 +0500

    feat:second vivo

---
+++ scripts/dependencies/dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+sudo apt-get update
+sudo apt-get install -y python3-tk python3-dev xclip dbus-x11 xvfb x11vnc xdotool
