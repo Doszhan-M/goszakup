d670d18cb38384cac702f0767c36acfabe41d2ea
Doszhan <doszhan.work@gmail.com>
2024-10-05 13:09:20 +0500

    fix:owner website

--- tender_pw/app/schemas/auth_scheme.py
+++ tender_pw/app/schemas/auth_scheme.py
@@ -9,7 +9,4 @@ class AuthScheme(BaseModel):
         + "/static/eds/GOST512_b79a7a0994373be5fb329692fd8f5774cddb6af8.p12"
     )
     eds_pass: str = "Aa1234"
-    goszakup_pass: str = "87014333488Aa"
-
-
-
+    goszakup_pass: str = "87014333488Aa@"
